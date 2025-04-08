360892cf250d071aaad61ee210e4f4ff1a20ece1
aydenl3 <soloyeti41@gmail.com>
2025-04-07 19:15:03 -0700

    completed experiement 1

--- experiment1/js/project.js
+++ experiment1/js/project.js
@@ -23,12 +23,73 @@ class MyProjectClass {
 }
 
 function main() {
-  // create an instance of the class
-  let myInstance = new MyProjectClass("value1", "value2");
-
-  // call a method on the instance
-  myInstance.myMethod();
+  const fillers = {
+    adventurer: ["person","law-abiding citizen","traveler","valued customer","adventurer","little goblin","hero"],
+    wares: ["item", "weapon", "thing", "loot", "treasure", "thingy","knick-knack"],
+    adjective: ["incredible", "amazing", "not-so-bad","probably useful", "wonderful", "interesting", "powerful", "extra discounted", "bargain day", "shiny"],
+    weapontype: ["Axe", "Longsword", "Bow", "Cloak", "Shield", "Club", "Crossbow", "Spear", "Boots", "Ring", "Cabbage", "Idol","Knife","Dagger","Darts","Throwing Stars","Rope","Spell","Flower"],
+    weaponname: ["of Mythos","of Power","the Destroyer","of Legend","","$weapontype","$weaponname $weaponname","the Mighty","of Cunning","of Might","of Doom","of Wonder"],
+    number: [1,2,3,4,5,6,7,8,9,],
+    dicenumber: [2,4,6,8,10,12],
+    damagetype:["slashing","piercing","bludgeoning","fire","vegetable","force","thunder","cold","poison","lightning","holy","necrotic"],
+    property:["is light","is heavy","must be loaded","has finesse","has reach","can be thrown","is angry","is truly epic","is two-handed"]
+  };
+  
+  const template = `Greetings $adventurer!
+  
+  Come, take a look at my $adjective $wares!
+  
+  $weapontype $weaponname.
+  
+  $number D$dicenumber $damagetype damage.
+  can attack up to $number 0 ft. away.
+  This weapon $property.
+  
+  
+  
+  `;
+  
+  const slotPattern = /\$(\w+)/;
+  
+  const itemlist = [];
+  
+  function replacer(match, name) {
+    let options = fillers[name];
+    if (options) {
+      let choice_picked = options[Math.floor(Math.random() * options.length)]
+      itemlist.push(choice_picked);
+      return choice_picked;
+    } else {
+      return `<UNKNOWN:${name}>`;
+    }
+  }
+  
+  function generate() {
+    itemlist.length = 0;
+    let story = template;
+    while (story.match(slotPattern)) {
+      story = story.replace(slotPattern, replacer);
+    }
+  
+    /* global box */
+    $("#box").text(story);
+    console.log(itemlist);
+    let cost = 1;
+    for(let i = 0; i <itemlist.length;i++){
+      let item = itemlist[i];
+      if (typeof(item) == "number"){
+        cost *= item
+      }
+    }
+    console.log(cost);
+    document.getElementById("costDisplay").innerText = `Cost: ${cost} gold`;
+  }
+  
+  /* global clicker */
+  $("#clicker").click(generate);
+  
+  generate();
 }
 
 // let's get this party started - uncomment me
-//main();
+main();
